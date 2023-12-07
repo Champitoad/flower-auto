7d206f55a9e8f0d056d8f42b60a7667758d84c52
Pablo Donato <pablo.donato@tuta.io>
2023-12-08 00:44:36 +0100

    Lifedeath trace

--- test/trace.ml
+++ test/trace.ml
@@ -2,10 +2,11 @@ open Prover
 open Flower
 open Utils
 
-type cycle = (garden * garden * garden)
+(* type cycle = (garden * garden * garden) *)
+type cycle = (garden * garden)
 type trace = cycle list
 
-let lifecycle_trace (t : gtree) : cycle =
+(* let lifecycle_trace (t : gtree) : cycle =
   pollination t;
   let p = deepcopy_gtree t in
   reproduction t;
@@ -23,17 +24,17 @@ let life_trace (g : garden) : garden * trace =
     trace := Array.append !trace [| lifecycle_trace !t |];
   done;
   gtree_to_garden !t,
-  !trace |> Array.(remove_at (length !trace - 1)) |> Array.to_list
+  !trace |> Array.(remove_at (length !trace - 1)) |> Array.to_list *)
 
-let string_of_trace (init : garden) (trace : trace) : string =
+(* let string_of_trace (init : garden) (trace : trace) : string =
   List.fold_left begin fun acc (p, r, d) ->
     let p = string_of_garden p in
     let r = string_of_garden r in
     let d = string_of_garden d in
     Printf.sprintf "⊢ %s\n⊢ %s\n⊢ %s\n%s\n" d r p acc
-  end (string_of_garden init) trace
+  end (string_of_garden init) trace *)
 
-let latex_prftree_of_trace (init : garden) (trace : trace) : string =
+(* let latex_prftree_of_trace (init : garden) (trace : trace) : string =
   List.fold_left begin fun acc (p, r, d) ->
     let p = latex_of_garden p in
     let r = latex_of_garden r in
@@ -60,6 +61,61 @@ let latex_array_of_trace (init : garden) (trace : trace) : string =
   \xstep{\mathtt{P}} &%s\\
   \xstep{\mathtt{R}} &%s\\
   \xstep{\mathtt{D}} &%s%s|} p r d acc
+    end trace ""
+  in
+  Printf.sprintf
+{|\begin{array}{rl}
+  &%s%s
+  \end{array}|} (latex_of_garden init) content *)
+
+
+let lifecycle_trace (t : gtree) : garden * garden =
+  pollination t;
+  let p = deepcopy_gtree t in
+  deathcycle t;
+  let r = deepcopy_gtree t in
+  (gtree_to_garden p, gtree_to_garden r)
+
+let life_trace (g : garden) : garden * trace =
+  let t = ref (garden_to_gtree g) in
+  let t' = ref (deepcopy_gtree !t) in
+  let trace = ref [| lifecycle_trace !t |] in
+  while not (eq_gtree !t !t') do
+    t' := deepcopy_gtree !t;
+    trace := Array.append !trace [| lifecycle_trace !t |];
+  done;
+  gtree_to_garden !t,
+  !trace |> Array.(remove_at (length !trace - 1)) |> Array.to_list
+
+let string_of_trace (init : garden) (trace : trace) : string =
+  List.fold_left begin fun acc (p, r) ->
+    let p = string_of_garden p in
+    let r = string_of_garden r in
+    Printf.sprintf "⊢ %s\n⊢ %s\n%s\n" r p acc
+  end (string_of_garden init) trace
+
+let latex_prftree_of_trace (init : garden) (trace : trace) : string =
+  List.fold_left begin fun acc (p, r) ->
+    let p = latex_of_garden p in
+    let r = latex_of_garden r in
+    Printf.sprintf
+{|\R[\mathtt{P}]
+  {\R[\mathtt{RD}]
+  {%s}
+  {%s}}
+  {%s}|} r p acc
+  end (latex_of_garden init) trace
+
+let latex_array_of_trace (init : garden) (trace : trace) : string =
+  let content =
+    List.fold_right begin fun (p, r) acc ->
+      let p = latex_of_garden p in
+      let r = latex_of_garden r in
+      Printf.sprintf
+{|
+  \\[1em]\hline\\
+  \xstep{\mathtt{P}} &%s\\
+  \xstep{\mathtt{RD}} &%s%s|} p r acc
     end trace ""
   in
   Printf.sprintf
